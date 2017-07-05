c1a8a170a67f2699c1a3db3476f578ff345b9f78
calvm022 <martinlcalvert@gmail.com>
2017-07-04 23:07:18 -0700

    Add a copy for statuses

---
+++ app/controllers/statuses_controller.rb
@@ -0,0 +1,45 @@
+class StatusesController < ApplicationController
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  def index
+    apps = Statuses.all.to_a
+    respond_to do |format|
+      format.json do
+        render json: { apps: apps }
+      end
+    end
+  end
+
+  def save
+    app = if id = app_service_params['_id']
+            AppService.find(id)
+          else
+            AppService.create(app_service_params)
+          end
+    if app.present?
+      render json: { saved: true, app: app.to_json }
+    else
+      render json: { saved: false, error: true }
+    end
+  end
+
+  def destroy
+    if app = AppService.find(destroy_id) and app.destroy!
+      render json: { deleted: true, errors: [] }
+    else
+      render json: { deleted: false, errors: app.errors }
+    end
+  end
+
+  private
+
+  def app_service_params
+    params.require(:app).permit(AppService.permitted_fields, uris: [], _id: {}).to_h
+  end
+
+  def destroy_id
+    request.headers['X-APP-ID']
+  end
+end
